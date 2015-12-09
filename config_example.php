6048807b4853418bc86aa75b7462fecf0e6a0418
Neal Shyam <neal@audioshocker.com>
2015-12-08 20:09:48 -0500

    add config file example

---
+++ config_example.php
@@ -0,0 +1,9 @@
+<?php
+	$consumer_key = '---';
+	// path.to should be the URL where your web app is hosted.
+	// eg. http://jshawl.com/callback.php
+	$redirect_uri = 'http://localhost:8888/callback.php';
+
+	//don't edit this until you've authenticated with pocket.
+	$access_token = 'YOUR_ACCESS_TOKEN_HERE';
+?>
